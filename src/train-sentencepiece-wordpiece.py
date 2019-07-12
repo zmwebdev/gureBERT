a9129b464e2e5edb836f47ad6446cba4bfdd883b
zmwebdev <zmwebdev@gmail.com>
2019-07-12 15:28:36 +0200

    train-sentencepiece-wordpiece.py --normalization_rule_name=identity

--- src/train-sentencepiece-wordpiece.py
+++ src/train-sentencepiece-wordpiece.py
@@ -53,7 +53,7 @@ def train(prefix=PREFIX, vocab_size=VOCABSIZE, ctl_symbols=CTLSYMBOLS):
     # https://github.com/google/sentencepiece/blob/d4dd947fe71c4fa4ee24ad8297beee32887d8828/python/sentencepiece_python_module_example.ipynb
     # https://github.com/allenai/scibert/blob/5d72d0ec50e2d3ebe971122f8b282278c210eccd/scripts/cheatsheet.txt
     # spm.SentencePieceTrainer.Train('--input=combined.out --model_prefix=100B_9999_cased --vocab_size=31000 --character_coverage=0.9999 --model_type=bpe --input_sentence_size=100000000 --shuffle_input_sentence=true')
-    command = f'--input={files_tokenized} --model_prefix={prefix} --vocab_size={vocab_size} --control_symbols={ctl_symbols} --character_coverage=1.0 --model_type=bpe --input_sentence_size=100000000 --shuffle_input_sentence=true'
+    command = f'--input={files_tokenized} --model_prefix={prefix} --vocab_size={vocab_size} --control_symbols={ctl_symbols} --character_coverage=1.0 --model_type=bpe --input_sentence_size=100000000 --shuffle_input_sentence=true --normalization_rule_name=identity'
 
     # --model_type=word
     #command = f'--input={files_tokenized} --model_prefix={prefix} --vocab_size={vocab_size} --control_symbols={ctl_symbols} --model_type=word --hard_vocab_limit=false'  # RuntimeError: Internal: /sentencepiece/src/trainer_interface.cc(498) [(trainer_spec_.vocab_size()) == (model_proto->pieces_size())]
